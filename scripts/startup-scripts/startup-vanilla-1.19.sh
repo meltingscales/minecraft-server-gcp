4a33425961b266fbf4da360362d5b2530fd53ea4
Henry Post <HenryFBP@gmail.com>
2022-07-01 20:42:40 -0400

    rename to start-server

--- scripts/startup-scripts/startup-vanilla-1.19.sh
+++ scripts/startup-scripts/startup-vanilla-1.19.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bash ~/minecraft/minecraft-server-gcp/template-server-vanilla-1.19/startup.bash
+bash ~/minecraft/minecraft-server-gcp/template-server-vanilla-1.19/start-server.bash
