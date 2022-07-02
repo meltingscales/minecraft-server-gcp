feddeacaf9cba261704fe8a0b4c707c4cf16672f
Henry Post <HenryFBP@gmail.com>
2022-07-01 21:45:38 -0400

    fix fileurl

--- templates/ftb-monster-1.1.2-1.6.4/bootstrap.bash
+++ templates/ftb-monster-1.1.2-1.6.4/bootstrap.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FILEURL=https://github.com/HenryFBP/minecraft-server-gcp/archive/refs/tags/Monster-1.1.2-1.6.4Server.zip.zip
+FILEURL=https://github.com/HenryFBP/minecraft-server-gcp/releases/download/monster-server-1.1.2-1.6.4/Monster-1.1.2-1.6.4Server.zip
 FILENAME=Monster-1.1.2-1.6.4Server.zip
 
 if [ ! -d mods/ ]; then
