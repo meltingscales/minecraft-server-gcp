530ace262a52da759f9bb60a5b448a9c7eeaa6bb
Henry Post <HenryFBP@gmail.com>
2022-07-01 20:09:44 -0400

    improve setup

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-MC_VERSION=1.19
-
 sudo apt-get install openjdk-17-jdk
 sudo apt-get install screen
 sudo apt-get install curl wget lynx
 
 mkdir ~/minecraft
 pushd ~/minecraft
-wget -O "minecraft_server.${MC_VERSION}.jar" https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar
-echo "eula=true" > eula.txt #hahahah....this is ILLEGAL >;3c
 
-ls -lash
-pwd
+git clone git@github.com:HenryFBP/minecraft-server-gcp.git
 
-java -jar "minecraft_server.${MC_VERSION}.jar"
+pushd ./minecraft-server-gcp
 
+./startup.bash
