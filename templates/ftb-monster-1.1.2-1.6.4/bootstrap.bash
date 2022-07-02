c375ba2be01f9b863b2d65cda6f8bdc3e55814a2
henryfbp <HenryFBP@gmail.com>
2022-07-01 22:32:10 -0400

    download and unzip :D

--- templates/ftb-monster-1.1.2-1.6.4/bootstrap.bash
+++ templates/ftb-monster-1.1.2-1.6.4/bootstrap.bash
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+# This downloads missing mods if we are running modded.
+
 FILEURL=https://github.com/HenryFBP/minecraft-server-gcp/releases/download/monster-server-1.1.2-1.6.4/Monster-1.1.2-1.6.4Server.zip
 FILENAME=Monster-1.1.2-1.6.4Server.zip
 
-if [ ! -d mods/ ]; then
-    echo "You lack mods/, downloading curseforge zip into this dir :)"
+if [ ! -d ./mods/ ]; then
+    echo "You lack dir ./mods/, downloading curseforge zip into this dir :)"
 
-    curl $FILEURL -o $FILENAME
+    if [ ! -f "${FILENAME}" ]; then
+        curl -L $FILEURL --output $FILENAME
 
-    Monster-1.1.2-1.6.4.zip
+        unzip $FILENAME
+    fi
 
 else 
     echo "You already have mods, not downloading curseforge zip."
